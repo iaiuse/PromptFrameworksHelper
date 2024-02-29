f69963b248d109a71ea39b439ced20dfbf4c1abf
iaiuse <160843322+iaiuse@users.noreply.github.com>
2024-02-29 14:41:10 +0800

    Update generate-prompthelper.js

--- scripts/generate-prompthelper.js
+++ scripts/generate-prompthelper.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const yaml = require('js-yaml');
 
 const frameworksDir = './frameworks';
 const outputFile = './prompthelper.js';
@@ -7,9 +8,26 @@ const frameworks = [];
 
 // Read each file in the frameworks directory
 fs.readdirSync(frameworksDir).forEach(file => {
-  if (path.extname(file) === '.json') {
-    const content = fs.readFileSync(path.join(frameworksDir, file));
-    frameworks.push(JSON.parse(content));
+  if (path.extname(file) === '.yml') {
+    const content = fs.readFileSync(path.join(frameworksDir, file), 'utf8');
+    // 解析YAML内容
+    const parsedYaml = yaml.load(yamlContent);
+    // 转换YAML内容到期望的JSON结构
+    const jsonStructure = {
+      [parsedYaml.name]: {
+        "name": `${parsedYaml.name}框架`,
+        "author": parsedYaml.author,
+        "description": parsedYaml.description,
+        "fields": parsedYaml.fields.reduce((acc, field) => {
+          const key = Object.keys(field)[0];
+          acc[key] = { "text": field[key].text };
+          return acc;
+        }, {})
+      }
+    };
+    // 准备要插入到JS文件中的字符串
+    const jsonString = JSON.stringify(jsonStructure, null, 2);
+    frameworks.push(JSON.parse(jsonString));
   }
 });
 
