8fcb6059bc307df093302122c83e519c18a01010
iaiuse <160843322+iaiuse@users.noreply.github.com>
2024-03-05 10:47:27 +0800

    Update generate-prompthelper.js

--- scripts/generate-prompthelper.js
+++ scripts/generate-prompthelper.js
@@ -12,17 +12,37 @@ fs.readdirSync(frameworksDir).forEach(file => {
     const yamlContent = fs.readFileSync(path.join(frameworksDir, file), 'utf8');
     // 解析YAML内容
     const parsedYaml = yaml.load(yamlContent);
+
+    // 转换fields数组中每个项的处理
+    const fieldsTransformed = parsedYaml.fields.map(field => {
+        const key = Object.keys(field)[0]; // 比如 "Role"
+        const value = field[key]; // 包含 text, default, type 等
+    
+        // 创建一个新对象，将原始值直接赋值
+        const newField = {
+            [key]: {
+                ...value, // 直接展开原对象
+            }
+        };
+    
+        // 如果原始text是数组，直接保留为数组，如果是文本，也直接保留
+        if (value.text) newField[key].text = value.text;
+        // 类似地处理default和type
+        if (value.default) newField[key].default = value.default;
+        if (value.type) newField[key].type = value.type;
+        if (value.info) newField[key].info = value.info;
+    
+        return newField;
+    });
+
+    
     // 转换YAML内容到期望的JSON结构
     const jsonStructure = {
       [parsedYaml.name]: {
         "name": `${parsedYaml.name}框架`,
         "author": parsedYaml.author,
         "description": parsedYaml.description,
-        "fields": parsedYaml.fields.reduce((acc, field) => {
-          const key = Object.keys(field)[0];
-          acc[key] = { "text": field[key].text };
-          return acc;
-        }, {})
+        "fields": fieldsTransformed
       }
     };
     // 准备要插入到JS文件中的字符串
